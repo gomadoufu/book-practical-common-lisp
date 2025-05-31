7e15bc0534f22fb1893a97c9838f3b503852c57a
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2025-06-01 04:44:43 +0900

    WIP

---
+++ pathname-portable-lib.lisp
@@ -0,0 +1,33 @@
+(defun component-present-p (value)
+  (and value (not (eql value :unspecific))))
+
+(defun directory-pathname-p (p)
+  (and
+   (not (component-present-p (pathname-name p)))
+   (not (component-present-p (pathname-type p)))
+   p))
+
+(defun pathname-as-directory (name)
+  (let ((pathname (pathname name)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (if (not (directory-pathname-p name))
+        (make-pathname
+         :directory (append (or (pathname-directory pathname)
+                                (list :relative))
+                            (list (file-namestrig pathname)))
+         :name nil
+         :type nil
+         :defaults pathname)
+        pathname)))
+
+(defun directory-wildcard (dirname)
+  (make-pathname
+   :name :wild
+   :type #-clisp :wild #+clisp nil
+   :defaults (pathname-as-directory dirname)))
+
+(defun list-directory (dirname)
+  (when (wild-pathname-p dirname)
+    (error "Can only list concrete directory names."))
+  (directory (directory-wildcard dirname)))
